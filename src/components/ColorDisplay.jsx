a7c205efff94e4e8c297c9bc95c3bf8d2dc9438c
Arohan Dutt <arohandutt@gmail.com>
2020-01-22 18:10:21 -0600

    init

---
+++ src/components/ColorDisplay.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ColorDisplay = (props) => {
+  return (
+    <div>
+      This is the color display
+    </div>
+  )
+}
+
+export default ColorDisplay;
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ColorDisplay from './components/ColorDisplay.jsx';
 
 const App = () => {
+
+  const appStyle = {
+    padding: '10px'
+  }
+
   return (
-    <div>This is React</div>
+    <div style={appStyle}>
+      <h1>Color Generator</h1>
+      <p>Generate an array of browser-friendly colors based on lines you draw.</p>
+      <ColorDisplay />
+    </div>
   )
 }
 
