76310c921846f3f996c41dbf133fc06a016f33bf
Arohan Dutt <arohandutt@gmail.com>
2020-03-11 12:13:23 -0500

    Production build V1

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.jsx',
-  mode: 'development',
+  mode: 'production',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
